ef54ca3c99265e18eb28b7e8294d843d00bbc784
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-19 15:13:53 +1100

    added capability to change resolution of grid

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const INITIAL_SIZE = 16;
 
+const INITIAL_SIZE = 16;
+const MAX_SIZE = 100;
+
+
+main()
+
+function main() {
+	initializeSquareSketchboard(INITIAL_SIZE);
+	setCellCountInputValue(INITIAL_SIZE);
+	setUpEventHandlers();
+}
+
+function setCellCountInputValue(num) {
+	const cellCountInput = document.querySelector("#cellCountInput");
+	cellCountInput.value = num;
+}
+
+function getCellCountInputValue() {
+	const cellCountInput = document.querySelector("#cellCountInput");
+	return cellCountInput.value	
+} 
 
 function initializeSquareSketchboard(rowCellCount) {
 
 	const sketchboard = document.querySelector("#sketchboard");
+	
+	// remove children if present
+	sketchboard.textContent = "";
 
 	// create enough square cells to fill sketchboard
 	for (let i = 0; i < rowCellCount ** 2; i++) {
@@ -25,11 +48,25 @@ function setUpEventHandlers() {
 			colourSquare(target);
 		}
 	})
+
+	// reset board on refresh button click, correct input if needed
+	const refreshButton = document.querySelector("#refresh");
+	refreshButton.addEventListener("click", () => {
+		let cellCount = getCellCountInputValue();
+		if (isNaN(cellCount) ||
+			+cellCount <= 0 ||
+			+cellCount > MAX_SIZE ||
+			Math.floor(+cellCount) !== +cellCount // only integer values
+		) {
+			setCellCountInputValue(INITIAL_SIZE);
+			initializeSquareSketchboard(INITIAL_SIZE);
+		} else {
+			initializeSquareSketchboard(+cellCount);
+		}
+	})
 }
 
 function colourSquare(square) {
 	square.style["background-color"] = "darkblue";
 }
 
-initializeSquareSketchboard(16);
-setUpEventHandlers();
